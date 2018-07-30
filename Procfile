de1c34ade9117609cc034150475221881f4cf2e4
Nicholas Rempel <nbrempel@gmail.com>
2018-07-29 20:11:01 -0700

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+clock: python main.py
---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+from subprocess import call
+
+import schedule
+
+
+def renew():
+    call(
+        [
+            'L=/usr/local/bin/flynn && curl -sSL -A "`uname -sp`" https://dl.flynn.io/cli | zcat >$L && chmod +x $L'
+        ]
+    )
+
+
+if __name__ == "__main__":
+    renew()
+    schedule.every(10).minutes.do(renew)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)

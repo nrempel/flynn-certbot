4703531ed7ba063cbc6daf64d407cd14544757d5
Nicholas Rempel <nbrempel@gmail.com>
2018-07-29 20:17:09 -0700

    update location of flynn-cli

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import schedule
 def renew():
     call(
         [
-            'L=/usr/local/bin/flynn && curl -sSL -A "`uname -sp`" https://dl.flynn.io/cli | zcat >$L && chmod +x $L'
+            'L=./flynn && curl -sSL -A "`uname -sp`" https://dl.flynn.io/cli | zcat >$L && chmod +x $L'
         ]
     )
 

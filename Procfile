c89500cf301964eeef1796f6165152cbb3b3a4b1
Nicholas Rempel <nbrempel@gmail.com>
2018-07-30 09:47:16 -0700

    register route

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-clock: bash setup.sh && watch -n 5 ./main.sh
+clock: export FLYNN_CMD="/app/flynn" && export DIGITAL_OCEAN_SECRET_PATH="/app/digitalocean.ini" && ./setup.sh && watch -n 5 ./main.sh
--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 # Install flynn-cli
-export FLYNN_CMD="/app/flynn"
 L="$FLYNN_CMD" && curl -sSL -A "`uname -sp`" https://dl.flynn.io/cli | zcat >$L && chmod +x $L
 
 # Add cluster
@@ -11,7 +10,50 @@ if [ "$CERTBOT_DNS_PLUGIN" != "digitalocean" ]; then
 fi
 
 # Create file needed for certbot
-export DIGITAL_OCEAN_SECRET_PATH="/app/digitalocean.ini"
 echo "dns_digitalocean_token = $DIGITAL_OCEAN_API_KEY" > "$DIGITAL_OCEAN_SECRET_PATH"
 chmod 600 "$DIGITAL_OCEAN_SECRET_PATH"
 
+# Get domains array
+DOMAINS_PAIRS_ARRAY=(${DOMAINS//,/ })
+CERTBOT_COMMAND_STRING=""
+for ROUTE_DOMAIN_PAIR in "${DOMAINS_PAIRS_ARRAY[@]}"
+do
+    # Get domain from route:domain pair
+    DOMAINS_ARRAY=(${ROUTE_DOMAIN_PAIR//:/ })
+    DOMAIN="${DOMAINS_ARRAY[1]}"
+    # Make a string like '-d <domain 1> -d <domain 2>
+    CERTBOT_COMMAND_STRING="-d $DOMAIN $CERTBOT_COMMAND_STRING"
+done
+
+certbot certonly \
+  --work-dir /app \
+  --config-dir /app/config \
+  --logs-dir /app/logs \
+  --agree-tos \
+  --no-eff-email \
+  --test-cert \
+  --dns-digitalocean \
+  --email "$EMAIL" \
+  --dns-digitalocean-credentials "$DIGITAL_OCEAN_SECRET_PATH" \
+  "$CERTBOT_COMMAND_STRING"
+
+
+for ROUTE_DOMAIN_PAIR in "${DOMAINS_PAIRS_ARRAY[@]}"
+do
+    # Get app name and domain from app:domain pair
+    DOMAINS_ARRAY=(${ROUTE_DOMAIN_PAIR//:/ })
+    
+    APP_NAME="${DOMAINS_ARRAY[0]}"
+    DOMAIN="${DOMAINS_ARRAY[1]}"
+    
+    ROUTE_ID=$("$FLYNN_CMD" -a "$APP_NAME" route | grep "$DOMAIN" | awk '{print $3}')
+
+    if [ -z "$ROUTE_ID" ]; then
+        echo "Cannot determine route id from '$ROUTE_DOMAIN_PAIR'"
+        exit 1
+    fi
+
+    "$FLYNN_CMD" -a $APP_NAME route update $ROUTE_ID \
+        --tls-cert /app/config/live/$DOMAIN-0001/fullchain.pem \
+        --tls-key /app/config/live/$DOMAIN-0001/privkey.pem
+done
